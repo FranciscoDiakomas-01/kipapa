8c6e65e0c1dfd16d1cf99aea057f068904d99fa3
Francisco <franciscodiakoma@gmail.com>
2024-12-14 05:04:31 +0100

    termiando

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,38 +1,53 @@
 import './index.css'
 import pizza from '../../assets/pngegg (9).png'
-import hambuger from '../../assets/pngegg (1).png'
 import image from '../../assets/bugger.jpg'
-import { FaArrowLeft, FaArrowRight, FaPaperPlane, FaShoppingCart} from 'react-icons/fa'
+import {  FaPaperPlane, FaShoppingCart} from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
-import bg2 from '../../assets/464745397_18022215284605621_2273606827089846785_n.jpg'
 import { getAllCategory } from "../../services/CategoryProduct.js";
 import chef from '../../assets/chef1.png'
 import { useState, useEffect } from 'react'
 import AOS from 'aos'
+import svg1 from '../../assets/undraw_delivery_address_re_cjca.svg'
+import svg2 from "../../assets/undraw_on_the_way_re_swjt.svg";
+import svg3 from "../../assets/undraw_online_groceries_a02y.svg";
 export default function Main() {
       const navigate = useNavigate()
-      const [Active , setActive] = useState(0)
-      const banners = [
-          {
+      const banners = {
               text: "O SABOR DE VERDADE É NO KIPAPA",
               cover: pizza,
               slogan: "Delicie a sua vida no Kipapa",
               bg: image,
               
-          },
-          {
-              text: "DELICIE A SUA VIDA SEMPRE NO KIPAPA",
-              cover: hambuger,
-              slogan: "Kipapa prioriza a sua satisfação",
-              bg: bg2,
-          },
+      }
+      const howWork = [
+        {
+          id: 1,
+          title: "Faça o Pedido",
+          description:
+            "Faça o seu pedido na nossa plataforma e desfrute da variedade de produto que oferecemos para si..",
+          icon: svg3,
+        },
+        {
+          id: 2,
+          title: "Localização",
+          description:
+            "Informe nos a sua localização ou permita que obtenhamos a sua localização de forma automática.",
+          icon: svg1,
+        },
+        {
+          id: 3,
+          title: "Entrega",
+          description:
+            "Monitore o estado da sua compra em tempo real para saber tudo sobre a mesma.",
+          icon: svg2,
+        },
       ];
       const [categorys, setCategorys] = useState([]);
       useEffect(() => {
         AOS.init({
           duration: 1000, // Duração da animação em milissegundos
           easing: "ease-in-out", // Função de timing
-          offset: 80, // Deslocamento em pixels
+          offset: 100, // Deslocamento em pixels
 
         });
         AOS.refresh();
@@ -47,37 +62,15 @@ export default function Main() {
       <section id="main">
         <article
           style={{
-            backgroundImage: `linear-gradient(#000000b8 , #000000b8) , url(${banners[Active].bg})`,
+            backgroundImage: `linear-gradient(#000000b8 , #000000b8) , url(${banners.bg})`,
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
           }}
         >
-          <button
-            onClick={() => {
-              if (Active > 0) {
-                setActive((prev) => prev - 1);
-              } else {
-                setActive(banners.length - 1);
-              }
-            }}
-          >
-            <FaArrowLeft />
-          </button>
-          <button
-            onClick={() => {
-              if (Active >= banners.length - 1) {
-                setActive(0);
-              } else {
-                setActive((prev) => prev + 1);
-              }
-            }}
-          >
-            <FaArrowRight />
-          </button>
           <aside>
-            <span>{banners[Active].slogan}</span>
-            <h1>{banners[Active].text}</h1>
+            <span>{banners.slogan}</span>
+            <h1>{banners.text}</h1>
             <button
               onClick={() => {
                 navigate("/product");
@@ -88,11 +81,7 @@ export default function Main() {
             </button>
           </aside>
           <aside>
-            <img
-              src={banners[Active].cover}
-              loading="lazy"
-              data-aos="zoom-in"
-            />
+            <img src={banners.cover} loading="lazy" data-aos="zoom-in" />
           </aside>
         </article>
         <article>
@@ -106,8 +95,7 @@ export default function Main() {
               pedido no menor tempo possível, sem abrir mão da qualidade. Além
               disso, contamos com um atendimento personalizado e diversas formas
               de pagamento para facilitar a sua vida. Faça seu pedido agora
-              mesmo pelo nosso site e aproveite nossas promoções
-              exclusivas!
+              mesmo pelo nosso site e aproveite nossas promoções exclusivas!
             </p>
             <button
               data-aos="fade-up"
@@ -129,6 +117,19 @@ export default function Main() {
                 <p>{ct.title}</p>
               </div>
             ))}
+        </article>
+
+        <article>
+          <h1>Como Fazer Um Pedido</h1>
+          <div>
+            {howWork.map((data) => (
+              <figure key={data.id} data-aos="flip-right">
+                <img src={data.icon} />
+                <h2>{data.title}</h2>
+                <p>{data.description}</p>
+              </figure>
+            ))}
+          </div>
         </article>
       </section>
     );
